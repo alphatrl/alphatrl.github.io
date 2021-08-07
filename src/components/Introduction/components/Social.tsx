0322566dffdf201f7fb809b983e19177e6398614
Amos Tan <alphatrl1997@gmail.com>
2021-08-07 21:32:15 +0800

    feat: add introduction
    * move index.html to index.tsx

---
+++ src/components/Introduction/components/Social.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Icon from '../../Icon';
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 32px);
+  grid-gap: 32px;
+  margin-top: 54px;
+  padding-bottom: 24px;
+`;
+
+const StyledIcon = styled.a`
+  cursor: pointer;
+  color: ${(props) => props.theme.socialIconColor};
+
+  :hover {
+    color: ${(props) => props.theme.linkColor};
+  }
+`;
+
+const Social: React.FC = () => {
+  return (
+    <Wrapper>
+      <StyledIcon href="https://github.com/alphatrl" target="_blank">
+        <Icon name="github" width={32} height={32} />
+      </StyledIcon>
+      <StyledIcon href="https://twitter.com/alphatrl" target="_blank">
+        <Icon name="twitter" width={32} height={32} />
+      </StyledIcon>
+      <StyledIcon href="mailto://hello@amostan.me">
+        <Icon name="email" width={32} height={32} />
+      </StyledIcon>
+    </Wrapper>
+  );
+};
+
+export default Social;
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const theme = {};
+export const theme = {
+  socialIconColor: '#5a5a5a',
+  linkColor: '#0575f8',
+};
 
 export const GlobalStyle = createGlobalStyle`
   body {
