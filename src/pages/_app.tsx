dfffadc125d1ba6233c5efeab8bcd47534cd4189
Amos Tan <alphatrl1997@gmail.com>
2021-08-08 13:42:54 +0800

    feat: nextjs static import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
+import '../styles.css';
+
 import type { AppProps } from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import { GlobalStyle, theme } from '../theme';
+import { theme } from '../theme';
 
 const App: React.FC<AppProps> = (props) => {
   const { Component, pageProps } = props;
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
   );
