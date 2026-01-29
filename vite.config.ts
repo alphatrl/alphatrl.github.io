8fcc4556898c0f388d6b57e837e448a195cacb6f
Amos Tan <alphatrl1997@gmail.com>
2026-01-30 01:29:11 +0800

    refactor(styles): Support tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    tailwindcss(),
+  ],
 });
