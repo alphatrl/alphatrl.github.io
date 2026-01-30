e366b0aedfa216ecaa1bde2007e38c2a7c9a542d
Amos Tan <alphatrl1997@gmail.com>
2026-01-30 15:47:38 +0800

    WIP: react-grid-layout

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import DefaultLayout from '../../layouts/DefaultLayout';
+import GridLayout from './components/GridLayout';
 
 const HomePage: React.FC = () => {
   return (
     <DefaultLayout title="Portfolio">
-      <div className="flex flex-col"></div>
+      <div className="flex flex-col justify-center items-center">
+        <GridLayout />
+      </div>
     </DefaultLayout>
   );
 };
